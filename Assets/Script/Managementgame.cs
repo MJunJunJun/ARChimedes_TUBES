c37d2add9229af252ec9761dc895be279aad300b
Muhammad Junaedi <49475719+MJunJunJun@users.noreply.github.com>
2022-07-22 00:09:02 +0700

    JUN
    CEK

---
+++ Assets/Script/Managementgame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managementgame : MonoBehaviour
+{
+    public int garam;
+    public Animator animator;
+    public GameObject garamBtn;
+    public GameObject txtAtas,txtTengah, txtBawah;
+    // Start is called before the first frame update
+    void Start()
+    {
+        garam = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TambahGaram()
+    {
+        garam++;
+        animator.Play("Garam");
+    }
+    public void Telur()
+    {
+        if (garam == 0)
+        {
+            animator.Play("Tenggelam");
+            txtBawah.SetActive(true);
+        }
+        else if (garam == 1)
+        {
+            animator.Play("Ditengah");
+            txtTengah.SetActive(true);
+        }
+        else if (garam >= 2)
+        {
+            animator.Play("diatas");
+            txtAtas.SetActive(true);
+        }
+        garamBtn.SetActive(false);
+    }
+}
---
+++ Assets/Script/SetupMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupMainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject materi;
+    public GameObject quit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        materi.SetActive(false);
+        quit.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
